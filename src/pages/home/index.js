04b4d9916bb674b38c1455eddd60c07e34399521
ErickJoestar <erickborquez@gmail.com>
2020-07-04 05:42:15 -0600

    Update homepage

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,50 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import SearchComponent from "../../components/search-component";
-import StyleContainer from "../../components/styled-container";
+import StyledContainer from "../../components/styled-container";
+import Button from "../../components/button";
 
 import homeBanner from "../../assets/images/home-banner.jpg";
+import homeBook from "../../assets/images/home-book.jpg";
+
 import praxiLabs from "../../assets/images/Praxilabs.png";
 import coSpaces from "../../assets/images/CoSpaces.png";
 import tinkercad from "../../assets/images/Tinkercad.jpg";
 
+import { ReactComponent as ScienceLogo } from "../../assets/icons/science.svg";
+import { ReactComponent as CodeLogo } from "../../assets/icons/code.svg";
+import { ReactComponent as Design3D } from "../../assets/icons/design3d.svg";
+import { ReactComponent as Electronics } from "../../assets/icons/electronics.svg";
+import { ReactComponent as AumentedRel } from "../../assets/icons/aumentedReality.svg";
+import { ReactComponent as VirtualRel } from "../../assets/icons/virtualReality.svg";
+
 import "./style.css";
 import "../../shared/fonts.css";
+
+const AREAS_DATA = [
+  { label: "Ciencias", name: "science", SvgComponent: ScienceLogo, link: "/" },
+  { label: "Codificación", name: "code", SvgComponent: CodeLogo, link: "/" },
+  { label: "Diseño 3D", name: "design3d", SvgComponent: Design3D, link: "/" },
+  {
+    label: "Electrónica",
+    name: "electronics",
+    SvgComponent: Electronics,
+    link: "/",
+  },
+  {
+    label: "Realidad Virtual",
+    name: "virtual-rel",
+    SvgComponent: VirtualRel,
+    link: "/",
+  },
+  {
+    label: "Realidad Aumentada",
+    name: "aumented-rel",
+    SvgComponent: AumentedRel,
+    link: "/",
+  },
+];
 
 const Home = () => {
   const handleInputSearch = () => {};
@@ -44,14 +79,14 @@ const Home = () => {
           ofrecerte solo lo mejor.
         </span>
         <div className="flex-container mg-bottom mg-top">
-          <StyleContainer color="#CC3333" className="home__lab-container">
+          <StyledContainer color="#CC3333" className="home__lab-container">
             <img
               src={praxiLabs}
               className="home__lab-icon home__lab-icon--praxi"
               alt="Praxilabs logo"
             />
-          </StyleContainer>
-          <StyleContainer
+          </StyledContainer>
+          <StyledContainer
             color="linear-gradient(to right, #E58B5D, #A63CDF)"
             className="home__lab-container"
           >
@@ -60,16 +95,52 @@ const Home = () => {
               className="home__lab-icon home__lab-icon--cospaces"
               alt="CoSpaces Logo"
             />
-          </StyleContainer>
-          <StyleContainer color="#22B35A" className="home__lab-container">
+          </StyledContainer>
+          <StyledContainer color="#22B35A" className="home__lab-container">
             <img
               src={tinkercad}
               className="home__lab-icon home__lab-icon--tinker"
               alt="TinkerCad Logo"
             />
-          </StyleContainer>
+          </StyledContainer>
         </div>
       </section>
+      <section className="home__section home__section--side">
+        <img
+          src={homeBook}
+          alt="Mujer sosteniendo libros"
+          className="home__side__image"
+        />
+        <div className="home__side__text-container">
+          <span className="home__side-title purple">
+            LOS MEJORES LABS VIRTUALES EN UN SOLO LUGAR Y A TU ALCANCE
+          </span>
+          <Link to="/nosotros">
+            <Button shaped gradient className="home__side__button">
+              ¡Conócenos!
+            </Button>
+          </Link>
+        </div>
+      </section>
+      <section className="home__section">
+        <h4 className="page-section-title purple">
+          Descubre nuestras
+          <br /> áreas de aprendizaje
+        </h4>
+        <div className="home__areas">
+          {AREAS_DATA.map(({ label, name, SvgComponent, link }) => (
+            <div className="home__areas__el--container">
+              <StyledContainer gradient className="home__areas__el">
+                <SvgComponent
+                  className={`home__areas__svg home__areas__svg--${name}`}
+                />
+              </StyledContainer>
+              <span>{label}</span>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="home__section home__section__secondary-banner"></section>
     </main>
   );
 };
